f60479e667a79da9b03dacaeb9129e4be7db5fc6
CHOSEONGJUN <lionjojo@naver.com>
2024-10-31 16:31:56 +0900

    init : Create Project - 프로젝트 설정 및 DB 설정

---
+++ src/main/java/com/sparta/oauth2_0/OAuth20Application.java
@@ -0,0 +1,13 @@
+package com.sparta.oauth2_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth20Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/oauth2_0/OAuth20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.oauth2_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
